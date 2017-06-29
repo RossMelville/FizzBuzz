8aa279187bbdc4db2d201dfcddee684534ee63b0
Ross Melville <rossmelville@hotmail.com>
2017-06-29 12:47:06 +0100

    Additional tests for fizz_buzz function, created and all tests passed

--- fizz_buzz_spec.rb
+++ fizz_buzz_spec.rb
@@ -20,8 +20,23 @@ def test_fizz_buzz_7()
   assert_equal("7", fizz_buzz(7))
 end
 
+def test_fizz_buzz_3000()
+  assert_equal("FizzBuzz", fizz_buzz(3000))
+end
 
+def test_fizz_buzz_777777()
+  assert_equal("Fizz", fizz_buzz(777777))
+end
 
+def test_fizz_buzz_0()
+  assert_equal("FizzBuzz", fizz_buzz(0))
+end
 
+def test_fizz_buzz_50()
+  assert_equal("Buzz", fizz_buzz(50))
+end
+def test_fizz_buzz_2()
+  assert_equal("2", fizz_buzz(2))
+end
 
 end
